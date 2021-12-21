775e4e7fb73d710bc885ab36d90217e6389a2364
Greaver <t4cpirates@hotmail.com>
2021-12-21 19:48:38 +0200

    DB/Bristling Fur Spellscript
    Add spellscript to Bristling Fur spell.

---
+++ sql/ashamane/world/2021_12_21_00_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` = 155835 AND `Scriptname` = "spell_dru_bristling_fur";
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (155835, "spell_dru_bristling_fur");
